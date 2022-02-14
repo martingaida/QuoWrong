5aa458cf454c29cf2eafded11517f8e2d5a98b23
Jesse Christensen <jtq.christensen@gmail.com>
2022-02-14 14:49:32 -0800

    finished database table structure

---
+++ db/migrations/20220214223429-create-answer-vote.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('AnswerVotes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: { model: 'Users'},
+        type: Sequelize.INTEGER
+      },
+      answerId: {
+        allowNull: false,
+        references: { model: 'Answers'},
+        type: Sequelize.INTEGER
+      },
+      upVote: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('answerVotes');
+  }
+};
