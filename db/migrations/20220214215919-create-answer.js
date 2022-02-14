42555b2c466085cc22dbf11937709f5b675a1c15
Steven Kleinberg <stevenkleinberg5@gmail.com>
2022-02-14 16:04:19 -0600

    added answers model and migration files

---
+++ db/migrations/20220214215919-create-answer.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Users"}
+      },
+      questionId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Questions"}
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Answers');
+  }
+};
