e79179424fb7e2bd866a955653a1547ab39aeb47
Steven Kleinberg <stevenkleinberg5@gmail.com>
2022-02-14 19:05:39 -0600

    created pug utils, pug page for sign up, and the beginings of the sighn  up router logic

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const {csrfProtection, asyncHandler} = require('./utils');
+const db = require('../db/models')
+const { User } = db;
+const bcrypt = require('bcryptjs');
+const { check, validationResult} = require('express-validator');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/sign-up', csrfProtection, (req, res, next) => {
+  const user = User.build();
+  res.render('sign-up', {
+    title: 'Sign Up!',
+    user,
+    csrfToken: req.csrfToken()
+  })
+})
 
 module.exports = router;
