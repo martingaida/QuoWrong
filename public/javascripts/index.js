e72c6cb422dac0c2027f0af53a6e7ee74fa879b3
Jesse Christensen <jtq.christensen@gmail.com>
2022-02-17 15:11:36 -0800

    Front end dom edit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,30 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+window.addEventListener('DOMContentLoaded', (event)=>{
+    
+    const editForms = document.querySelectorAll('#edit-form')
+    
+    editForms.forEach(form => {
+        form.addEventListener('submit', async (e) => {
+
+            e.preventDefault();
+            const formData = new FormData(form);
+            const answerId = formData.get('answerId');
+            const questionId = formData.get('questionId');
+            const content = formData.get('content');
+
+            const res = await fetch(`/api/edit/${answerId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    questionId,
+                    content,
+                    answerId
+                })
+            });
+            const data = await res.json()
+            console.log(data)
+        })
+
+    })
 })
---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const { Answer } = require('../db/models');
+const { requireAuth } = require('../auth');
+const { asyncHandler, answerValidators } = require('./utils'); //CHANGE AND CREATE ANSWER VALIDATORS
+const { validationResult } = require('express-validator');
+
+
+
+router.put('/edit/:id(\\d+)', requireAuth, answerValidators, asyncHandler( async (req, res, next) => {
+
+  const { questionId, content, answerId } = req.body
+
+  const validatorErrors = validationResult(req);
+
+
+  if (validatorErrors.isEmpty()) {
+
+    const answer = await Answer.findByPk(req.params.id);
+    answer.content = content;
+    await answer.save();
+
+
+    res.json({data: answer});
+
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+
+    res.render('question', {
+      title: 'Edit',
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }
+}))
+
+module.exports = router;
