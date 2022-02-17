c568d902d2168b5f21e4c0a27e017bb6ccb5d035
Steven Kleinberg <stevenkleinberg5@gmail.com>
2022-02-17 10:34:12 -0600

    fixed the little error

--- routes/utils.js
+++ routes/utils.js
@@ -1,5 +1,4 @@
-const csrf = require('csurf')
-const csrfProtection = csrf({ cookie:true });
+
 const { check, validationResult} = require('express-validator');
 const db = require('../db/models');
 
